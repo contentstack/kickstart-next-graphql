b561031a6efe99fa951ba7dd346dbabceae7edf0
Tim Benniks <tbenniks@gmail.com>
2025-07-31 21:26:55 +0200

    chore: update dependencies and version bump to 1.0.5

--- codegen.ts
+++ codegen.ts
@@ -2,11 +2,11 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const apiKey = process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY;
-const environment = process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT;
-const region = process.env.NEXT_PUBLIC_CONTENTSTACK_REGION
+const apiKey = process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY!;
+const environment = process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT!;
+const region = process.env.NEXT_PUBLIC_CONTENTSTACK_REGION!;
 const baseURL = region === 'EU' ? 'eu-graphql.contentstack.com' : 'graphql.contentstack.com'
-const accessToken = process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN
+const accessToken = process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN!;
 
 const config: CodegenConfig = {
   schema: {
